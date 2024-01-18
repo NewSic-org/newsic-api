39283618f97493da3a640e094ea32e658e574184
gSayak <gsayak22@gmail.com>
2024-01-18 19:33:41 +0530

    cors2

--- api/newsic-api.py
+++ api/newsic-api.py
@@ -8,7 +8,7 @@ from pinecone import Pinecone
 from openai import OpenAI
 
 app = Flask(__name__)
-CORS(app, )
+CORS(app, resources={r"/*": {"origins": "https://newsic-frontend.vercel.app", "methods": ["GET", "POST", "OPTIONS"]}})
 
 env_path = pathlib.Path('..') / '.local.env'
 load_dotenv(dotenv_path=env_path)
